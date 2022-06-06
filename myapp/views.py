f094e66a0ecf12c6fb8454c6fd2a24d1735b97a4
sklee1216 <54343936+sklee1216@users.noreply.github.com>
2022-06-06 16:20:29 +0900

    submit

--- myapp/views.py
+++ myapp/views.py
@@ -8,28 +8,49 @@ topics = [
 {'id':3,'title':'model','body':'Model is ..'},
 ]
 
-def index(request):
+def HTMLTemplate(articleTag):
+
     global topics
     ol = ''
     for topic in topics:
         ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
-
-    return HttpResponse(f'''
+    return f'''
     <html>
-
     <body>
-        <h1>Hello, Django!</h1>
+        <h1><a href="/">Hello, Django!</h1>
         <ol>
             {ol}
         </ol>
-        <h2>Welcome</h2>
-        Hello, Django
+        {articleTag}
+        <ul>
+        <li><a href="/create/"> Create </a></li>
+        </ul>
         </body>
         </html>
-    ''')
+    '''
+
+def index(request):
+    article ='''
+    <h2>Welcome</h2>
+        Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def create(request):
-    return HttpResponse('Create')
+    article = '''
+        <form action = "/create/">
+        <p><input type="text" name = "title" placeholder="title"></p>
+        <p><textarea name = "body" placeholder = "body"></textarea></p>
+        <p><input type = "submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 def read(request,id):
-    return HttpResponse('Read' + id) 
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+
+    return HttpResponse(HTMLTemplate(article))
